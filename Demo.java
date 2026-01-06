5a70ae3e21c39690d92432c96e83e64898af894f
ashutoshs007 <Ashutosh.12222528@lpu.in>
2026-01-07 02:06:02 +0530

    getterandsetter

--- Demo.java
+++ Demo.java
@@ -8,11 +8,19 @@ public  int getAge()
     return age;
 }
 
+public void setAge( int a){
+    age= a;
+}
 
 
 public String getName()
 {
     return name;
+}
+
+public void setName(String n)
+{
+    name = n;
 }
 
 }
@@ -25,6 +33,9 @@ public class Demo
       Human obj = new Human();
     //   obj.age = 11;
     //   obj.name="Navin";
+    obj.setAge(23);
+    obj.setName("Ashutosh");
+
  
     //   System.out.println(obj.name);   //won't work coz name is private so to access it you need to access via method which we made inside that class
   
