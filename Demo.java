32db456b300d1fa480320a16b29d47471ef27dc0
ashutoshs007 <Ashutosh.12222528@lpu.in>
2026-01-07 18:42:45 +0530

    Demo

--- Demo.java
+++ Demo.java
@@ -1,35 +1,18 @@
-//how to differentiate between instance variable and local variable
 
-//this is a keyword which represents current object(the object which is calling the method , this refers to the current object which is calling setAge )
-
-// ->by passing the object itself
 
  class Human{
 
-private int age ;  //this is accessible in the same class
+private int age ; 
 private String name ;
+
 public  int getAge(){
     return age;
 }
 
-// public void setAge( int a){
-//     age= a;
-// }
-
-// public void setAge( int age){
-// public void setAge( int age,Human obj){
 
 public void setAge( int age){
-    // Human obj1=new Human(); 
-    // obj1.age =age;
-
-
-//     Human obj1 = obj;
-//    obj1. age= age;
-
-this.age=age;
+    this.age =age;
 }
-
 
 public String getName(){
     return name;
