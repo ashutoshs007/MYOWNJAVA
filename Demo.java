f2d7f97ea0ff970aaf9a9b693883ad1b71d240ee
ashutoshs007 <Ashutosh.12222528@lpu.in>
2026-01-09 00:46:49 +0530

    changes

--- Demo.java
+++ Demo.java
@@ -1,26 +1,24 @@
+class A
+{
+
+    public A()
+    {
+        System.out.println("object created");
+    }
+    public void show()
+    {
+        System.out.println("in A show");
+    }
+}
+
 public class Demo
 {
     public static void main(String a[])
     {
 
+        int marks;
+        marks =99;
+        A obj =new A();
+        obj.show();
     }
 }
-
-
-/*Naming Convention in Java */
-//Camel casing
-
-//class and interface -Calc ,Runable(interface)
-//variable and method - marks,show()
-
-//constant =PIE,BRAND
-
-//showMyMarks()   //it is called camelcasing
-
-//show_my_marks() //it also works but not preferable
-
-// MyData  (classname)
-
-// Human()     //called constructor
-
-
