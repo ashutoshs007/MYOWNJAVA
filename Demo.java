9b79e47eefd3e3076b76df26eadd74776dd880fe
ashutoshs007 <Ashutosh.12222528@lpu.in>
2026-01-12 02:40:29 +0530

    parameterizedsuper

--- Demo.java
+++ Demo.java
@@ -1,6 +1,23 @@
 // super method simply means -call the constructor of a superclass
 
 
+// Every class in java extends Object class even if we don't mention (john cina meme-it's there but you can't see it meme)
+
+
+
+
+// both below line works the same
+// coz super in A  below it is also having some superclass called Object to which class A  is extending
+
+
+// super() in A means calling a constructor of the Object class
+
+
+// class A extends Object
+
+// but B  class is stil lnot extending both A and Object class,cause java do not support multiple inheritence,it's still like multilevel inheritence, where class B extends A and A extends Object Class.
+
+
 
 
 class A
@@ -37,8 +54,9 @@ class B extends A
      //parameterized constructor
      public B(int n){
 
-        super();
-        
+        // super();
+        super(n);
+
 
            System.out.println("in B int");
      }
@@ -55,6 +73,7 @@ public class Demo
     public static void main(String args[])
     {
        B obj =new B(69); 
+    //    B obj =new B(); 
        //WHENEVER YOU CREATE AN OBJECT IT WILL CALL THE CONSTRUCTOR OF A CLASS
 
        //it will print both 
@@ -63,5 +82,10 @@ public class Demo
     //    as well as superclass(A).
 
     // Every constructor in java has a method(even if you don't mention there that method still exists)called super();
+
+
+    //Every class in java extends the object class
+
+    //Object is a class in java which has lots of methods
     }
 }
