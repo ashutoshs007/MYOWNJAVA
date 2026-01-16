12f1f74e20bd8177c9b5895e459a55cb2d53a785
ashutoshs007 <Ashutosh.12222528@lpu.in>
2026-01-16 19:35:21 +0530

    final

--- Demo.java
+++ Demo.java
@@ -1,46 +1,17 @@
-//final keyword -can be used with variable,method,class
-
-
-
-// making a variable as final means making it constant
-//making a class as final means stopping inheritence
-
-//if you made your method final,no method can override it
-
-// final class Calc
-class Calc
+class Laptop
 {
-    public final void show()
-    {
-        System.out.println("By Navin");
-    }
-
-
-    public void add(int a,int b)
-    {
-        System.out.println(a+b);
-    }
+    String model;
+    int price;
 }
-
-class AdvCalc extends Calc
-{
-
-
-    public void show()
-    {
-        System.out.println("By John");
-    }
-}
-
 
 public class Demo
 {
     public static void main(String a[])
     {
+       Laptop obj=new Laptop();
+       obj.model ="Lenevo Yoga";
+       obj.price=1000;
 
-   AdvCalc obj = new AdvCalc();
-   obj.show();
-   obj.add(4,5);
-
-    }
+       System.out.println(obj);
+   }
 }
