c93c1bd1d9fd237251d112bc59dc9733c2ffaae1
ashutoshs007 <Ashutosh.12222528@lpu.in>
2026-01-18 03:00:58 +0530

    objectclass.java

--- Demo.java
+++ Demo.java
@@ -1,17 +1,32 @@
+//Every class in java extends the object class
+
 class Laptop
 {
-    String model;
-    int price;
+ int price;
+ String model;
+
+
+ public String toString()
+ {
+  return model + " : "+price;
+ }
+ 
 }
 
 public class Demo
 {
-    public static void main(String a[])
-    {
-       Laptop obj=new Laptop();
-       obj.model ="Lenevo Yoga";
-       obj.price=1000;
+    public static void main(String [] args){
 
-       System.out.println(obj);
-   }
+Laptop obj = new Laptop();
+  obj.model ="Lenovo Yoga";
+  obj.price=1000;
+
+
+  System.out.println(obj);
+  System.out.println(obj.toString());//Everytime you try to print the object ,it will call toString method even if you don't mention.
+
+   System.out.println(obj); //toString getting called by default
+ 
+}
+
 }
