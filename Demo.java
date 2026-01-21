3ab356d7c3dd6ba03bc4a56d79acbd17f36d62f8
ashutoshs007 <Ashutosh.12222528@lpu.in>
2026-01-21 19:40:59 +0530

    need of  reference

--- Demo.java
+++ Demo.java
@@ -1,63 +1,59 @@
 
-// abstract class A
+
+// abstract class Computer
 // {
 
-
-//     // In abstract class we can create abstract methods as well as normal methods
-
-//     public abstract void show();
-//     public abstracrt void config();
+//     abstract public void code();
+    
 // }
 
-
-
-
-//interface is not a class
-//every method in the interface is public abstract
-
-interface A
+interface Computer
 {
 
-// all the variables inside interface are by default final and static
-
-
-    int age =44 ; 
-    String area="Mumbai";
-
-    void show();
-    void config();
+     void code();
+    
 }
 
 
-//whenever you say implements ,it become compulsary for you to define both the methods,if you failed to define both the methods by default the class B also becomes abstract
 
 
 
-
-//you only get methods in implements not variables
-
-
-//interface do not have their own memory in the heap,so if you do not have your own memory how you can have  a  non-final variable,that's why you only get methods in implements not variables
-
-
-class B implements A 
-
+// class Laptop extends Computer
+class Laptop implements Computer
 {
-
-    public void show()
+    public void code()
     {
-        System.out.println("in show");
+        System.out.println("code,compile,run");
     }
-
-
-    public void config()
-    {
-        System.out.println("in config");
-    }
-
 }
 
 
+// class Desktop extends Computer
+class Desktop implements Computer
+{
+    public void code()
+    {
+        System.out.println("code,compile,run:Final");
+    }
+}
+
+
+
+
+
+
+//we are doing tight coupling here by saying that developer is dependent on laptop only and if he got desktop someday ,he cannot work ,but no developer is depended on a Computer ,a Computer can be a Laptop ,it can be a Desktop as well
+
+class Developer
+{
+    // public void devApp(Laptop lap)
+
+    public void devApp(Computer lap)
+    {
+        // System.out.println("coding..");
+        lap.code();
+    }
+}
 
 
 
@@ -66,21 +62,24 @@ public class Demo
     public static void main(String a[])
     {
 
-     A obj ;
-    //  obj =new A(); // A is abstract; cannot be instantiated
+        // Laptop lap =new Laptop();
 
-    //  you cannot create object of A but you can create object of B
+        Computer  lap =new Laptop();   //interface reference not class reference
+
+        // Desktop desk =new Desktop();
+
+        Computer desk =new Desktop();
+
+        Developer navin = new Developer();
+
+        // navin.devApp(lap);
+        navin.devApp(lap);
+        navin.devApp(desk);
 
 
-    obj =new B();
-    obj.show();
-    obj.config();
-
-    //Since age and area are static ,we can directly use the interface name  ,when we use static varaible ,we don't have to use the object
-
-    // A.area="Bangalore";   cannot assign a value to static final variable area
-
-
-    System.out.println(A.area);//we can directly do this without creating an object
     }
 }
+
+
+
+//so that's how you design application to make it loosly coupled where your developer object is not dependent on Laptop or Desktop ,it is depended on Computer
