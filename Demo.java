fe8b637dacb8762a5f09d5de36d0a90018ead5ec
ashutoshs007 <Ashutosh.12222528@lpu.in>
2026-01-21 19:17:21 +0530

    interface

--- Demo.java
+++ Demo.java
@@ -1,21 +1,57 @@
-//Anonymous inner class
 
 
-abstract class A 
+// When you have a class which has only abstract methods ,the other alternative  for that is to create an interface(we use mostly in project development) 
+
+// Interface is not a class
+
+// by default every method in a interface 
+// is public abstract by default
+
+
+
+
+// abstract class A
+// {
+
+//     public abstract void show();  //public abstract is there by default
+//     public abstract void config();
+// }
+
+
+
+interface A
 {
-    public  abstract void show();
-   
+
+
+    //Every variable in the interface are bydefault dinal and static
+
+    int age=44 ;
+    String area ="Mumbai";
+    
+    void show();
+    void config();
 }
 
+// implements means by default it is compulsary for you to define both the methods ,if you failed to do so then this class is also becomes abstract by default
 
+//interface do not have their own memory in the heap
 
-class B extends A{
+class B implements A
+{
 
-    public void show(){
-    System.out.println("in B show");
+    public void show()
+    {
+        System.out.println("in show");
     }
 
-}
+    public void config()
+    {
+         System.out.println("in config");
+    } 
+    }
+
+
+
 
 
 
@@ -26,25 +62,12 @@ public class Demo
     public static void main(String a[])
     {
 
-    //    A obj =new A();    //won't work coz. you cannot instantiate 
-        // the abstract class so just extend A using B then provide implementation there 
-    
+       A obj ;
+    //    obj = new A();  //A is abstract; cannot be instantiated
 
-
-    //    A obj =new B(); 
-    //    obj.show(); 
-
-       A obj = new A()  //here we can do this coz. we are creating the object of anonymous ineer class
-       {
-
-         public void show()
-         {
-    System.out.println("in new show");
-    }
-       };
-
+       obj =new B();
        obj.show();
+       obj.config();
+
     }
 }
-
-
