11fe6f70b0097767bdbad372ed61a17a51631067
ashutoshs007 <Ashutosh.12222528@lpu.in>
2026-01-25 01:10:31 +0530

    Annotations

--- Demo.java
+++ Demo.java
@@ -1,35 +1,20 @@
-enum Laptop{
+class A
+{
+ public void showTheDataWhichBelongsToThisClass()
+   {
+    System.out.println("in A show");
+   }
+}
 
-    // 4 objects will call constructor 4 times
-    //one time default constructor will be called and 3 times parametarized constructor will be called
+class B extends A
+{
 
-
-    Macbook(2000) ,XPS(2200),Surface,ThinkPad(1800);
-
-//    that's how private constructor works where we can create the object in the same class
-
-
-
-    private int price;
-
-    private Laptop(){
-        price=500;
-    }
-
-    private Laptop(int price){
-        this.price=price;
-        System.out.println(" in Laptop "+this.name());
-    }
-
-
-    public int getPrice(){
-        return price;
-    }
-
-    public void setPrice(int price){
-        this.price=price;
-    }
-
+    @Override
+//    public void showTheDataWhichBelongToThisClass()
+   public void showTheDataWhichBelongsToThisClass()
+   {
+    System.out.println("in  B show");
+   }
 }
 
 
@@ -39,20 +24,7 @@ public class Demo
     public static void main(String a[])
     {
 
-
-        // Laptop lap = Laptop.Macbook;
-        // System.out.println(lap + " : " +lap.getPrice());
-
-
-        //need all laptops price
-
-        for(Laptop lap: Laptop.values())
-        {
-            System.out.println(lap + " : " +lap.getPrice());
-
-        }
-
-
-
+     B obj = new B();
+     obj.showTheDataWhichBelongsToThisClass();
     }
 }
