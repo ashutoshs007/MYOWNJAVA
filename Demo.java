861be7d45d03a7ed0a4595987151ed5e3a32dbe5
ashutoshs007 <Ashutosh.12222528@lpu.in>
2026-02-04 21:58:45 +0530

    throwkeyword

--- Demo.java
+++ Demo.java
@@ -1,11 +1,10 @@
 public class Demo{
     public static void main(String a[]){
 
-int i=2 ;
+int i=20 ;
 int j=0 ;
 
-int nums[] =new int[5];
-String str = null;
+
 
 
 
@@ -13,23 +12,22 @@ String str = null;
 try
 {
     j=18/i;
-    System.out.println(str.length());
-    System.out.println(nums[1]);
-    System.out.println(nums[5]);
+    if(j==0)
+        throw new ArithmeticException("I do not want to print zero"); //calling catch block using throw 
+
+    //throw keyword is used to throw the exceptino and catch is used to catch it.
+
+   
 }
-catch( ArithmeticException e)
-{
-System.out.println("Cannot divide by  zero ");   //  ArithmeticException  
+catch( ArithmeticException e){
+// System.out.println("Cannot divide by  zero ");   //  ArithmeticException  
+j=18/1;
+System.out.println("that's the default output"+e);
 }
 
-catch( ArrayIndexOutOfBoundsException e)
-{
-System.out.println("Stay in your limit ");   //  ArrayIndexOutOfBoundsException 
-}
 
-catch(Exception e)
-{
-    System.out.println("Stay in your limit.");
+catch(Exception e){
+    System.out.println("Stay in your limit."+e);
 }
 
 System.out.println(j);
